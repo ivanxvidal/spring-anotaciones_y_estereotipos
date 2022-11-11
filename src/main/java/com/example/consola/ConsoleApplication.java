346f7cbe18643d2b86c1f94a66774533265a8e4b
ivan_vidal <ivanxvidal@gmail.com>
2022-11-10 22:42:45 -0600

    Ejercicio usando Qualifier

--- src/main/java/com/example/consola/ConsoleApplication.java
+++ src/main/java/com/example/consola/ConsoleApplication.java
@@ -3,17 +3,22 @@ package com.example.consola;
 import com.service.IPersonaService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.service"})// Revisa los modulos(paquetes) externos a este.
+@ComponentScan({"com.service", "com.extra"})// Revisa los modulos(paquetes) externos a este.
 public class ConsoleApplication implements CommandLineRunner {
 
     @Autowired
     private IPersonaService service;
+
+    @Autowired
+    @Qualifier("archivo_extra")
+    private com.extra.IPersonaService service2;
 
     org.slf4j.Logger LOG = LoggerFactory.getLogger(ConsoleApplication.class);
 
@@ -27,6 +32,8 @@ public class ConsoleApplication implements CommandLineRunner {
         LOG.warn("Hola desde la ternimal >:)");
         //service = new PersonaServiceImpl(); //Usando la anotacion @Autowired ya no es necesario la instancia
         service.registrar("Juan Carlos Bodoque");
+//        service2 = new PersonaServiceImpl();
+        service2.registrar("Tulio Triviño");
     }
 
 }
